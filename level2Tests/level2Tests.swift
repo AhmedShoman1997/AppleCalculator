af251a495253be5e04e182f1bf75309247a71391
ahmed <ahmed>
2020-09-15 08:53:59 +0200

    Initial Commit

---
+++ level2Tests/level2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  level2Tests.swift
+//  level2Tests
+//
+//  Created by Ahmed Shoman on 9/15/20.
+//  Copyright © 2020 Ahmed Shoman. All rights reserved.
+//
+
+import XCTest
+@testable import level2
+
+class level2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
